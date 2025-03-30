ea459e55f388d9541640037506316044f0ad58b6
Rapina <rapina.sv04@gmail.com>
2025-03-30 14:41:54 -0300

    sistema de colisão e detecção de interação com a porta

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 // DECLARAR VARIAVEIS
 const camera = document.getElementById("camera");
 var rotate = 0;
-var x = 250;
+var x = 700;
 var y = 150;
-var z = -500;
+var z = -1000;
 const acceptedkeys = ["KeyW", "KeyA", "KeyS", "KeyD"];
 var keys = [];
 var mouseX = 0;
 var mouseY = 0;
 var speed = 1.5;
+var playerPosition = "room"
+const door = document.getElementsByClassName("door");
+var canInteractDoor = false;
 
 
 
@@ -32,19 +35,21 @@ document.addEventListener("mousemove", (event) => {
 })
 
 
-
-
-
+// PLAYER CODE
 const playerCode = setInterval(() => {
+    // ROTATE
+    let rotating = false;
     if (mouseX > 130 || mouseX < -130) {
         rotate -= mouseX/400;
         rotate = rotate > 360 ? 0 : rotate;
         rotate = rotate < 0 ? 360 : rotate;
+        rotating = true;
     }
 
     let moving = false;
     let radians = rotate * (Math.PI / 180);
 
+    // MOVE
     if (keys.includes("KeyW")) {
         x -= Math.sin(radians) * (speed * 1.5);
         z += Math.cos(radians) * (speed * 1.5);
@@ -69,15 +74,47 @@ const playerCode = setInterval(() => {
         moving = true;
     };
 
-    if (x > 15 || x < 22) {
-        console.log("fora da sala")
+    // UPDATE CAMERA
+    if (moving || rotating) {
+        camera.style.transform = `perspective(1000px) translateY(${y}px) translateZ(1000px) rotateX(90deg) rotateZ(${rotate}deg) translate(${-(x)}px, ${z}px)`;
     }
-
-    camera.style.transform = `perspective(1000px) translateY(${y}px) translateZ(1000px) rotateX(90deg) rotateZ(${rotate}deg) translate(${-(x)}px, ${z}px)`;
-
+    // MOVING ANIMATION
     if (moving) {
         camera.style.animation = "moving-animation 0.7s infinite"
     } else {
         camera.style.animation = ""
     }
-}, 10);
+
+    // ROOM COLISION 
+    if (playerPosition == "room") {
+        if (x < 470) {
+            x = 470;
+        } else if (x > 875) {
+            x = 875;
+        }
+        if (z > -490) {
+            z = -490;
+        } else if (z < -1175) {
+            z = -1175;
+        }
+    }
+    // DOOR 
+    if (x > 500 && x < 625 && z > -1000 && z < -825) {
+        console.log("caninteractdoor")
+    }
+    
+}, 14);
+camera.style.transform = `perspective(1000px) translateY(${y}px) translateZ(1000px) rotateX(90deg) rotateZ(${rotate}deg) translate(${-(x)}px, ${z}px)`;
+
+
+
+
+
+
+
+
+
+
+// AUDIOS
+const ambience = new Audio("assets/audio/ambience.mp3");
+ambience.volume = 0.2;
